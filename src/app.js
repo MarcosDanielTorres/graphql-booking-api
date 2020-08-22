8ed66c97c3a21d4efaabbf5f1a3b278610e5c7b7
MarcosDanielTorres <marcoss.nqn@gmail.com>
2020-08-22 12:42:00 -0300

    changed variable name port to uppercase

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`App is running on port: ${port}.`);
+app.listen(PORT, () => {
+  console.log(`App is running on port: ${PORT}.`);
 });
